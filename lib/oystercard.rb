1a6855aadeba4f17f173bfd84e4b3855aca01883
Carlos <chdezmar@gmail.com>
2016-05-30 13:27:14 +0100

    Up to Challenge 5 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,13 @@
 class Oystercard
+
+attr_accessor :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(value)
+    @balance += value
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,25 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  context "balance" do
+    it "has a balance of 0 by default" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  context "transaction" do
+
+    describe "#top_up" do
+
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+
+      it "tops up when method #top_up is invoked" do
+        subject.balance = 0
+        subject.top_up(10)
+        expect(subject.balance).to eq 10
+      end
+    end
+  end
+
 end
